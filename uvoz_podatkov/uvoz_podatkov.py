69e374af3c586802d382148b7dbf2972d8a002d8
Ziga <ziggaaa257@gmail.com>
2019-05-11 14:40:37 +0200

    dodane tabele

--- uvoz_podatkov/uvoz_podatkov.py
+++ uvoz_podatkov/uvoz_podatkov.py
@@ -105,22 +105,83 @@ def zapisi_podatke_v_csv(list_of_dicts_ads, directory, filename):
 #    shrani_stran(url, 'uvoz_podatkov\\spletne_strani', '{}.html'.format(znamka))
 
 
+
 directory = 'uvoz_podatkov\\tabele'
 
 # AUDI
-
 regularni_audi = r'(?P<znamka>\w+)\s+(?P<model>\w+).*</span>.*'
-zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\\spletne_strani', 'Audi.html', regularni_audi), directory, 'Audi.csv')
-
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Audi.html', regularni_audi), directory, 'Audi.csv')
 # BMW
-
 bmw_regularni = r'(?P<znamka>\w{3})\s.*?:\s(?P<model>(\w|\d)+)\s.*?</span>.*?'
-zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\\spletne_strani', 'BMW.html', bmw_regularni), directory, 'BMW.csv')
-
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'BMW.html', bmw_regularni), directory, 'BMW.csv')   
 # Citroen
-
 citr_reg = r'(?P<znamka>Citroen)\s(?P<model>(\w|\d)+)\s.*?</span>.*?'
-zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\\spletne_strani', 'Citroen.html', citr_reg), directory, 'Citroen.csv')
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Citroen.html', citr_reg), directory, 'Citroen.csv')
+# Peugeot
+peug_reg = r'(?P<znamka>Peugeot)\s(?P<model>(\d+))\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Peugeot.html', peug_reg), directory, 'Peugeot.csv')
+#Renault
+renault_reg = r'(?P<znamka>Renault)\s(?P<model>(\w+))\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Renault.html', renault_reg), directory, 'Renault.csv')
+#Opel
+opel_reg = r'(?P<znamka>Opel)\s(?P<model>(\w+))\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Opel.html', opel_reg), directory, 'Opel.csv')
+#Mercedes-Benz
+marcedes_reg = r'(?P<znamka>Mercedes-Benz)\s(?P<model>\w+-\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Mercedes-Benz.html', marcedes_reg), directory, 'Mercedes-Benz.csv')
+#Ford
+ford_reg = r'(?P<znamka>Ford)\s(?P<model>\w+-?\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Ford.html', ford_reg), directory, 'Ford.csv')
+#Volkswagen 
+volkswagen_reg = r'(?P<znamka>Volkswagen)\s(?P<model>\w+-?\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Volkswagen.html', volkswagen_reg), directory, 'Volkswagen.csv')
+#Volvo
+volvo_reg = r'(?P<znamka>Volvo)\s(?P<model>\w+?\d+?)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Volvo.html', volvo_reg), directory, 'Volvo.csv')
+#Toyota
+toyota_reg = r'(?P<znamka>Toyota)\s(?P<model>\w+\d*?)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Toyota.html', toyota_reg), directory, 'Toyota.csv')
+#Fiat
+fiat_reg = r'(?P<znamka>Fiat)\s(?P<model>\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Fiat.html', fiat_reg), directory, 'Fiat.csv')
+#Suzuki
+suzuki_reg = r'(?P<znamka>Suzuki)\s(?P<model>\w+-?\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Suzuki.html', suzuki_reg), directory, 'Suzuki.csv')
+#Nissan
+nissan_reg = r'(?P<znamka>Nissan)\s(?P<model>\w+-?\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Nissan.html', nissan_reg), directory, 'Nissan.csv')
+#Honda
+honda_reg = r'(?P<znamka>Honda)\s(?P<model>\w+-?\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Honda.html', honda_reg), directory, 'Honda.csv')
+#Kia
+kia_reg = r'(?P<znamka>Kia)\s(?P<model>\w+-?\w+)\s.*?</span>.*?'
+zapisi_podatke_v_csv(pridobi_slovar('uvoz_podatkov\spletne_strani', 'Kia.html', kia_reg), directory, 'Kia.csv')
 
+#Spremeniti je potrebno locilo v CSV
+csv_dir = os.getcwd()
+dir_tree = os.walk(csv_dir)
+for dirpath, dirnames, filenames in dir_tree:
+   pass
 
-# SPLETNE STRANI SO ŽE POBRANE ZA ZAPIS CSV RABIŠ SAMO POGLEDAT KAKO JE ZAPISAN MODEL AVTA IN PRILAGODIŠ REGULARNI IZRAZ
+csv_list = []
+for file in filenames:
+   if file.endswith('.csv'):
+      csv_list.append(file)
+
+for tabela in csv_list:
+    reader = csv.reader(open('uvoz_podatkov/tabele/' + str(tabela), 'rU'), delimiter=',')
+    writer = csv.writer(open('uvoz_podatkov/temp/' + str(tabela), 'w'), delimiter=';')
+    writer.writerows(reader)
+
+#ZDRUZEVANJE TABEL ??? tole ne dela
+import os
+import glob
+import pandas as pd
+
+csv_dir = os.getcwd()
+extension = 'csv'
+all_filenames = [i for i in glob.glob('*.{}'.format(extension))]
+
+vozila = pd.concat([pd.read_csv(f) for f in csv_list])
+
+vozila.to_csv("vozila.csv", index=False, encoding = 'utf-8-sig')
