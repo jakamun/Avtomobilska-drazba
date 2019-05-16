f52a08008dca8eb1bc6cdfc9e0f51cb15acc4ae6
Jan Rudof <jan.rudof@student.fmf.uni-lj.si>
2019-05-16 13:18:00 +0200

    sql datoteka

--- shema_drazba.sql
+++ shema_drazba.sql
@@ -1,38 +1,50 @@
 CREATE TABLE IF NOT EXISTS oseba (
-    id INTEGER PRIMARY KEY
+    id_oseba INTEGER PRIMARY KEY
     ime TEXT NOT NULL
     priimek TEXT NOT NULL
     rojstvo DATE NOT NULL
-    kraj TEXT NOT NULL
-    trr INTEGER UNIQUE NOT NULL
+    racun INTEGER UNIQUE NOT NULL
     je_cenilec BOOLEAN NOT NULL
+    kraj TEXT NOT NULL
     ocena INTEGER
     cena INTEGER 
-)
-CREATE TABLE IF NOT EXISTS uporabnik (
-    id INTEGER REFERENCES oseba(id)
-    geslo TEXT NOT NULL
+    
 )
 
 CREATE TABLE avtomobil (
-    serijska_stevilka INTEGER PRIMARY KEY
+    id_avto INTEGER PRIMARY KEY
+    id_model INTEGER REFERENCES model(model_id)
+    prevozeni_kilometri INTEGER NOT NULL
     gorivo TEXT NOT NULL
-    zacetna_cena INTEGER 
-    dejanska_vrednost INTEGER NOT NULL
     velikost_motorja INTEGER NOT NULL
-    kW INTEGER NOT NULL   
+    kW INTEGER NOT NULL 
+    cena INTEGER NOT NULL
+    zacetna_cena INTEGER NOT NULL    
 )
 
 CREATE TABLE znamka (
-    serijska_stevilka INTEGER REFERENCES avtomobil(serijska_stevilka)
+    id_znamka INTEGER PRIMARY KEY 
     znamka TEXT NOT NULL
 )
 
 CREATE TABLE model (
-    serijska_stevilka INTEGER REFERENCES avtomobil(serijska_stevilka)
+    id_model INTEGER REFERENCES avtomobil(serijska_stevilka)
     model TEXT NOT NULL
+    id_znamka INTEGER REFERENCES znamka(id_znamka)
+)
+
+CREATE TABLE ocena (
+    id_ocena INTEGER PRIMARY KEY AUTOINCREMENT
+    avto INTEGER REFERENCES avtomobil(id_avto)
+    kupec INTEGER REFERENCES oseba(id_oseba)
+    cenilec INTEGER REFERENCES oseba(id_oseba)
+    vrednost INTEGER NOT NULL    
 )
 
 CREATE TABLE ponudba (
-    
+    id_ponudba INTEGER PRIMARY KEY AUTOINCREMENT
+    ponudnik INTEGER REFERENCES oseba(id_oseba)
+    avto INTEGER REFERENCES avtomobil(id_avto)
+    cena INTEGER NOT NULL
+
 )
