643e6dd23a3cca191f2e9dc36a6f3aa3ea744d1d
Jaka Munda <jaka.munda@siol.net>
2019-05-23 13:32:20 +0200

    se ne povezana registracija

--- baza.py
+++ baza.py
@@ -11,6 +11,9 @@ import auth_public as auth
 import psycopg2, psycopg2.extensions, psycopg2.extras
 psycopg2.extensions.register_type(psycopg2.extensions.UNICODE) # se znebimo problemov s šumniki
 
+# Skrivnost za kodiranje cookijev
+secret = "to skrivnost je zelo tezko uganiti 1094107c907cw982982c42"
+
 # odkomentiraj, če želiš sporočila o napakah
 # debug(True)
 
@@ -19,10 +22,97 @@ def static(filename):
     return static_file(filename, root='static')
 
 
-@get('/')
-def index():
-    cur.execute("SELECT * FROM oseba ORDER BY priimek, ime")
-    return template('osebe.html', osebe=cur)
+#@get('/')
+#def index():
+#    cur.execute("SELECT * FROM oseba ORDER BY priimek, ime")
+#    return template('osebe.html', osebe=cur)
+
+
+def password_md5(s):
+    """Vrni MD5 hash danega UTF-8 niza. Gesla vedno spravimo v bazo
+       kodirana s to funkcijo."""
+    h = hashlib.md5()
+    h.update(s.encode('utf-8'))
+    return h.hexdigest()
+
+
+def get_user(auto_login = True):
+    """Poglej cookie in ugotovi, kdo je prijavljeni uporabnik,
+       vrni njegov username in ime. Če ni prijavljen, presumeri
+       na stran za prijavo ali vrni None (advisno od auto_login).
+    """
+    # Dobimo username iz piškotka
+    username = request.get_cookie('username', secret=secret)
+    # Preverimo, ali ta uporabnik obstaja
+    if username is not None:
+        c = conn.cursor()
+        c.execute("SELECT username FROM uporabnik WHERE username=?",
+                  [username])
+        r = c.fetchone()
+        c.close ()
+        if r is not None:
+            # uporabnik obstaja, vrnemo njegove podatke
+            return r
+    # Če pridemo do sem, uporabnik ni prijavljen, naredimo redirect
+    if auto_login:
+        redirect('/login/')
+    else:
+        return None
+
+@get("/")
+def main():
+    """Glavna stran."""
+    # Iz cookieja dobimo uporabnika (ali ga preusmerimo na login, če
+    # nima cookija)
+    (username) = get_user()
+    # Vrnemo predlogo za glavno stran
+    return template("main.html",
+                           username=username)
+
+
+@get("/login/")
+def login_get():
+    """Serviraj formo za login."""
+    return template("login.html",
+                           napaka=None,
+                           username=None)
+
+
+@get("/register/")
+def login_get():
+    """Prikaži formo za registracijo."""
+    return template("register.html", 
+                           username=None, napaka=None)
+
+
+@post("/register/")
+def register_post():
+    """Registriraj novega uporabnika."""
+    username = request.forms.username
+    password1 = request.forms.password1
+    password2 = request.forms.password2
+    # Ali uporabnik že obstaja?
+    c = conn.cursor()
+    c.execute("SELECT 1 FROM uporabnik WHERE username=?", [username])
+    if c.fetchone():
+        # Uporabnik že obstaja
+        return template("register.html",
+                               username=username,
+                               napaka='To uporabniško ime je že zavzeto')
+    elif not password1 == password2:
+        # Geslo se ne ujemata
+        return template("register.html",
+                               username=username,
+                               napaka='Gesli se ne ujemata')
+    else:
+        # Vse je v redu, vstavi novega uporabnika v bazo
+        password = password_md5(password1)
+        c.execute("INSERT INTO uporabnik (username, password) VALUES (%s, %s)",
+                  (username, password))
+        # Daj uporabniku cookie
+        response.set_cookie('username', username, path='/', secret=secret)
+        redirect("/")
+
 
 ######################################################################
 # Glavni program
